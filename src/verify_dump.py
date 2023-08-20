3ecc1d4662daf26d7fd290a1f862c01accddffbe
HenrikBaltazar <henrikgomesbaltazar@gmail.com>
2023-08-20 17:42:33 -0300

    feat python plus test

---
+++ src/verify_dump.py
@@ -0,0 +1,33 @@
+import sys
+
+###############################
+
+if len(sys.argv) > 1:
+    file = sys.argv[1]  
+else:
+    file = input("Digite o nome do arquivo: ")
+
+###############################
+
+binario_e_CPI = []
+
+try:
+    with open(file, 'r') as openFile:
+        for cpi, binario in enumerate(openFile, start=1):
+            binario_e_CPI.append((binario.strip(), cpi))
+
+###############################
+
+    print("CPI por instrução:")
+    for binario, cpi in binario_e_CPI:
+        print(f"{binario} | CPI: {cpi}")
+
+    ciclosTotais = sum(num for linha, num in binario_e_CPI if num.isnumeric())
+    print(f"Ciclos totais: {ciclosTotais}")
+
+###############################
+
+except FileNotFoundError:
+    print(f"O arquivo '{file}' não foi encontrado.")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
