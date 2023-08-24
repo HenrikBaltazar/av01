12d5878a6f113a77b550423d1eb07c333ce7a38c
prrThr <arthur.op9@gmail.com>
2023-08-24 15:02:35 -0300

    getClocks updated #1

--- src/verify_dump.py
+++ src/verify_dump.py
@@ -1,32 +1,46 @@
 import sys
 
+rType = "000000"
+iType = ["001000","001001","001100","001101","001111", "001010", "001011", "001110"]
+jType = ["000010", "000011"]
+loadStore = ["100011", "101011"]
+branch = ["000100", "000101"]
+
+
+### TODO: Alterar a função abaixo (getClocks) para que os clocks sejam de acordo com:
+### rType e derivados = 4 clocks
+### iType = 1 clock
+### jType = 3 clocks
+### loadStore = 5 clocks
+### branch = 3 clocks
 def getClocks(binary):
     opcode = binary[0 : 6]
-    return opcode
+    if opcode == "000000" or opcode == "001000" or opcode == "001101" or opcode == "001111":
+        clocks = 1
+    elif opcode == "000010" or opcode == "000101":
+        clocks = 2
+    else:
+        clocks = 5
+    return clocks
 
 ###############################
 
 if len(sys.argv) > 1:
     file = sys.argv[1]  
 else:
-    file = input("Type the filename: ")
+    file = input("Type the filename (full path): ")
 
 ###############################
 
 binaryAndClocks = []
 
 try:
-    with open(f"dump_files/{file}", 'r') as openFile:
+    with open(file, 'r') as openFile:
         for clocks, binary in enumerate(openFile, start=1):
-            if getClocks(binary.strip()) == "000000" or getClocks(binary.strip()) == "001000" or getClocks(binary.strip()) == "001101" or getClocks(binary.strip()) == "001111":
-                binaryStrip = 4
-            elif getClocks(binary.strip()) == "000010" or getClocks(binary.strip()) == "000101":
-                binaryStrip = 3
-            else:
-                binaryStrip = 5
-            binaryAndClocks.append((binary.strip(), binaryStrip))
+            binaryAndClocks.append((binary.strip(), getClocks(binary.strip())))
 
-    with open(f"dump_files/{file}", 'r') as openFile:
+#### É possível ter o total de linhas do arquivo sem precisar desse código a mais?
+    with open(file, 'r') as openFile:
         line_count = sum(1 for line in openFile)
 
 ###############################
