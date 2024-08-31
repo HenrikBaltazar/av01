56c73dfb337bd885fb8bf3bdec61965ca4b5de7f
HenrikBaltazar <henrikgomesbaltazar@gmail.com>
2024-08-30 21:19:43 -0300

    fix depois dos comentarios do Douglas

--- src/verify_dump.py
+++ src/verify_dump.py
@@ -6,37 +6,38 @@ def getType(binary):
     rType = "000000"
     iType = ["001000","001001","001100","001101","001111", "001010", "001011", "001110"]
     jType = ["000010", "000011"]
-    loadStore = ["100011", "101011"]
+    loadType = ["100011", "100001","100101", "100000", "100100"]
+    storeType = ["101011", "101001", "101000"]
     branch = ["000100", "000101"]
     opcode = binary[:6]
     if opcode in iType:
         return "I"
     elif opcode in jType:
         return "J"
-    elif opcode in loadStore:
-        return "LS"
+    elif opcode in loadType:
+        return "L"
+    elif opcode in storeType:
+        return "S"  
     elif opcode in branch:
         return "BR"
-    elif binary == "00000000000000000000000000000000":
-        return "nop"
     elif opcode in rType:
         return "R"
     else:
         return "undefined"
 
-###############################
+############################### 
 
 def getClocks(binary):
     if getType(binary) == "I":
         return 4
     elif getType(binary) == "J":
         return 3
-    elif getType(binary) == "LS":
+    elif getType(binary) == "L":
         return 5
+    elif getType(binary) == "S":
+        return 4
     elif getType(binary) == "BR":
         return 3
-    elif getType(binary)== "nop":
-        return 1
     elif getType(binary) == "R":
         return 4
     else:
@@ -65,7 +66,7 @@ try:
     for binary, clocks in binaryAndClocks:
         print(f"{binary[:6]} {binary[6:]} | Type: {getType(binary)} | Clock cycles: {clocks}")
 
-    totalCycles = sum(clocks for binary, clocks in binaryAndClocks)
+    totalCycles = sum(clocks for _, clocks in binaryAndClocks)
     cpi = totalCycles / totalInstructions
     cpi = str(round(cpi, 2))
     print("\n\033[1m"+"RESULT:"+"\033[0m"+f"\nTotal Instructions: {totalInstructions}\nTotal cycles: {totalCycles}\nCPI: {cpi}\n")
@@ -76,4 +77,3 @@ except FileNotFoundError:
     print(f"File '{file}' not found.")
 except Exception as e:
     print(f"Unknown error: {e}")
-    
